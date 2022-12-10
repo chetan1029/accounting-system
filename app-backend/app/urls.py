c221370d5411e4c0591eaa0f4c3499a2e5c91bba
Chetan Singh <chetan.singh@ericsson.com>
2022-12-10 17:25:12 +0100

    Backend: Implementing accounts api endpoint

--- app-backend/app/urls.py
+++ app-backend/app/urls.py
@@ -1,23 +1,10 @@
-"""app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""app URL Configuration"""
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ping', views.ping, name='ping'),
+    path('api/accounting/accounts', views.AccountListView.as_view()),
+    path('api/accounting/accounts/<account_id>', views.AccountDetailsView.as_view())
 ]
--- app-backend/app/views.py
+++ app-backend/app/views.py
@@ -1,5 +1,17 @@
-from django.http import JsonResponse
+from .models import Account
+from .serializers import AccountSerializer
+from django.db.models import F
+from rest_framework import generics
+from rest_framework import filters
 
+class AccountListView(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
 
-def ping(request):
-    return JsonResponse({"result": "pong"})
+    filter_backends = [filters.OrderingFilter]
+    ordering = ['-created_at']
+
+class AccountDetailsView(generics.RetrieveAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    lookup_field = "account_id"
